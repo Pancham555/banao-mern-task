f1750a7415bd7f71c9bcebe8daa8b592f9b45498
Pancham <{panchamb63@gmail.com}+{Pancham555}@users.noreply.github.com>
2024-06-16 10:46:05 +0530

    Added recommended groups

--- src/components/location-tab.tsx
+++ src/components/location-tab.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { MdEdit, MdOutlineLocationOn } from "react-icons/md";
 import { IoInformationCircleOutline } from "react-icons/io5";
+import Image from "next/image";
+import { Badge } from "./ui/badge";
 const LocationTab = () => {
   return (
     <div className="flex justify-center w-full mt-5">
@@ -22,9 +24,46 @@ const LocationTab = () => {
             experience.
           </span>
         </div>
+        <div className="mt-10">
+          <p className="font-medium text-xl mb-2">👍 Recommended Groups</p>
+          <div className="flex flex-col gap-2">
+            <FollowUser />
+            <FollowUser />
+            <FollowUser followed />
+            <FollowUser followed />
+            <FollowUser followed />
+          </div>
+          <div className="flex justify-end my-4">
+            <p className="text-blue-500 cursor-pointer font-medium">
+              See more...
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
+const FollowUser = ({ followed }: { followed?: boolean }) => {
+  return (
+    <div className="flex justify-between py-2">
+      <div className="flex items-center gap-2">
+        <Image
+          src={"/assets/img1.jpg"}
+          alt={"profile"}
+          width={40}
+          height={40}
+          className="w-10 h-10 rounded-full"
+        />
+        <p className="font-medium">User name</p>
+      </div>
+      <Badge
+        className="cursor-pointer px-3 text-base"
+        variant={followed ? "secondary" : "default"}
+      >
+        {followed ? "Following" : "Follow"}
+      </Badge>
+    </div>
+  );
+};
 export default LocationTab;
